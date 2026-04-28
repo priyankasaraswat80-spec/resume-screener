19284aa1a92f39b694f180f5fd507c0269166de1
priyankasaraswat80-spec <priyankasaraswat80@gmail.com>
2026-04-28 11:34:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import re
+import os
 
 app = Flask(__name__)
 
@@ -9,13 +10,13 @@ def clean_text(text):
     return text
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     score = None
 
-    if request.method == 'POST':
-        resume = request.form['resume']
-        job = request.form['job']
+    if request.method == "POST":
+        resume = request.form["resume"]
+        job = request.form["job"]
 
         resume = clean_text(resume)
         job = clean_text(job)
@@ -23,11 +24,18 @@ def index():
         resume_words = set(resume.split())
         job_words = set(job.split())
 
-        match = len(resume_words & job_words)
-        score = round((match / (len(job_words)+1)) * 100, 2)
+        
+        match = resume_words.intersection(job_words)
 
-    return render_template('index.html', score=score)
+        if len(job_words) != 0:
+            score = int((len(match) / len(job_words)) * 100)
+        else:
+            score = 0
 
-# 🔹 Run app
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("index.html", score=score)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
